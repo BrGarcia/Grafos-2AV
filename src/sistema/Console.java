f54ee55fd6ef2a4ede43d6bdc69ff43a9036238a
Bruno Garcia <brunodog@gmail.com>
2016-06-21 17:13:49 -0300

    inici da ediccao dos menus
    17:13
    21/06/16

--- src/sistema/Console.java
+++ src/sistema/Console.java
@@ -1,5 +1,6 @@
 package sistema;
 
+import java.io.IOException;
 import java.util.Scanner;
 import grafo.*;
 import algoritmos.*;
@@ -8,11 +9,20 @@ public class Console {
 
 	public static void main(String[] args) {
 		MenuPrograma menu = new MenuPrograma();
-
+		Grafo grafoEmUso;
 		int verticeId, arestaId, verticeOrigem, verticeDestino, ax = 0, peso;
 
-		menu.menuInicial();
+
+		ax = menu.menuInicial();
+
+
+		do{
+			switch(ax){
+			case 1: break;
+			default : break;
+			}
+
+		}while(ax != 777);
 
 	}
-
 }
--- src/sistema/MenuPrograma.java
+++ src/sistema/MenuPrograma.java
@@ -1,12 +1,40 @@
 package sistema;
-
-
 import java.util.Scanner;
 
 
 public class MenuPrograma {
-	
+
 	public int menuInicial(){
+		System.out.println("MATCOMP - GRAPHANALYSIS");
+		System.out.println("MENU");
+		System.out.println("1 - Criar Grafo");
+		System.out.println("2 - Criar Vértice");
+		System.out.println("3 - Criar Aresta");
+		System.out.println("4 - Deletar Grafo ");
+		System.out.println("5 - Deletar Vértice");
+		System.out.println("6 - Deletar Aresta");
+		System.out.println("7 - Relatorio");
+		System.out.println("8 - Ferramentas");
+		System.out.println("9 - Carregar Exemplos");
+		System.out.println("0 - Fim");
+			
+		int opcao = new Scanner(System.in).nextInt();
+		switch(opcao) {
+		case 1: return menuCriarGrafo();
+		case 2:	return 2;
+		case 3: System.out.println("3 - Criar Aresta"); 		break;
+		case 4: System.out.println("4 - Deletar Grafo "); 		break;
+		case 5: System.out.println("5 - Deletar Vértice"); 		break;
+		case 6: System.out.println("6 - Deletar Aresta"); 		break;
+		case 7: System.out.println("7 - Relatorio"); 			break;
+		case 8: System.out.println("8 - Ferramentas"); 			break;
+		case 9: System.out.println("9 - Carregar Exemplos"); 	break;
+		default : return 777;
+		}
+		return 777;
+	}
+
+	public int menuCriarGrafo(){
 		System.out.println("Grafos!");
 		System.out.println();
 		System.out.println("grafo será ordenado?");
@@ -15,8 +43,14 @@ public class MenuPrograma {
 		System.out.println();
 		System.out.println("(Obs. Informar Apenas 0 ou 1)");
 		int opcao = new Scanner(System.in).nextInt();
+		switch(opcao) {
+		case 0: return 10;
+		case 1: return 11;
+		default: System.out.println("Opção inválida"); return 777;
+		}
+	}
+	public int menuTerciario(){
+		int opcao = new Scanner(System.in).nextInt();
 		return opcao;
 	}
-
-
 }
